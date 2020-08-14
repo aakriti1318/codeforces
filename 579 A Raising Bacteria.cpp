7cc2d6f22c44422ce5d681822c4066c8cf17b948
aakriti1318 <56245613+aakriti1318@users.noreply.github.com>
2020-08-14 23:22:16 +0530

    Add files via upload

---
+++ 579 A Raising Bacteria.cpp
@@ -0,0 +1,11 @@
+//https://codeforces.com/problemset/problem/579/A
+// 579 A Raising Bacteria
+
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    int ans =  __builtin_popcount(n);
+    cout<<ans<<endl;
+}
