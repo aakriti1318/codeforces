ee65524ab51c461389bb5331f118bcc14c63709f
aakriti1318 <56245613+aakriti1318@users.noreply.github.com>
2020-09-12 14:35:21 +0530

    Add files via upload

---
+++ 1186 A. Vus the Cossack and a Contest.cpp
@@ -0,0 +1,12 @@
+//https://codeforces.com/problemset/problem/1186/A
+// 1186 A. Vus the Cossack and a Contest
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int n,m,k;
+int main() {
+    cin>>n>>m>>k;
+    if(n<=m &&  n<=k) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}
