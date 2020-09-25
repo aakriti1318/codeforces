4886111afe53b858554a5982333459a76dfdde3e
aakriti1318 <56245613+aakriti1318@users.noreply.github.com>
2020-09-25 22:57:40 +0530

    Add files via upload

---
+++ 1300A - Non-zero.cpp
@@ -0,0 +1,43 @@
+//https://codeforces.com/problemset/problem/1300/A
+// 	1300A - Non-zero
+
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    for(int i=0;i<t;i++){
+        int n;
+        cin>>n;
+        int arr[n];
+        int zero=0;
+        int sum=0;
+        int total=0;
+        for(int j=0;j<n;j++){
+            cin>>arr[j];
+            if(arr[j]==0){
+                zero++;
+            }
+            sum=sum+arr[j];
+        }
+        
+        if(zero==0){
+            if(sum==0){
+                cout<<"1"<<endl;
+            }
+            else{
+                cout<<"0"<<endl;
+            }
+        }
+        else{
+            total=zero;
+            sum=sum+zero;
+            if(sum==0){
+                cout<<total+1<<endl;
+            }
+            else{
+                cout<<total<<endl;
+            }
+        }
+    }
+}
